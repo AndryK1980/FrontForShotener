b12e3088899b03d431b3239d0f2a87e9267f29a7
AndryK <a.kriganovsky@gmail.com>
2018-06-09 15:57:30 +0300

    added show short url

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ $(document).ready(function(){
 var API_URL = 'http://localhost:4000'
 var sendUrl = function(){
     var url = $('#url').val();
-    var shotUrlInput=$('#shot').val();
+    var shotUrlInput=$('#short');
     $.ajax({
         url: API_URL + '/urls',
         method: 'POST',
         dataType: 'json',
         data: {url: url},
         success: function(res){
-            console.log(res.body);
-            shotUrlInput=res;
+            console.log(res.short_url);
+            shotUrlInput.attr('href',res.short_url);
+            shotUrlInput.text(res.short_url)
         },
         error: function(err){
             console.log(err)
