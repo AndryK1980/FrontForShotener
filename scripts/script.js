7765f1fb1d4f454066879a8682b2f8f90926b42d
AndryK <a.kriganovsky@gmail.com>
2018-06-11 14:19:24 +0300

    added botstrap and modal window

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('#btn-shorten').on('click', sendUrl);
+});
+var API_URL = 'http://localhost:4000'
+var sendUrl = function(){
+    var url = $('#url-field').val();
+    var userShortUrl=$('#short-url-field').val();
+    var shotUrlInput=$('#short-url-link');
+    var validErr=$('#massege-err');
+    $.ajax({
+        url: API_URL + '/urls',
+        method: 'POST',
+        dataType: 'json',
+        data: {url: url,userShortUrl:userShortUrl},
+        success: function(res){
+            //console.log(res.short_url);
+            shotUrlInput.attr('href',res.short_url);
+            shotUrlInput.text(res.short_url)
+        },
+        error: function(err){
+            validErr.text(err.responseJSON.message);
+            modalErr();
+        }
+
+    })
+};
+
+var modalErr=function () {
+        $("#myModalErr").modal('show');
+};
